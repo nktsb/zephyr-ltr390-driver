ca4a6f79db39b49457c43387af640a6f9c38d13a
nktsb <ny.subbotin@yandex.ru>
2025-06-12 14:28:45 +0300

    Added init sequence

--- src/ltr390.c
+++ src/ltr390.c
@@ -10,6 +10,9 @@
 #include <zephyr/logging/log.h>
 #include <zephyr/kernel.h>
 
+#define UV_COUNT       160
+#define UV_SENSITIVITY 2300 
+
 LOG_MODULE_REGISTER(ltr390, CONFIG_SENSOR_LOG_LEVEL);
 
 static int ltr390_write_byte(const struct device *dev, uint8_t reg, uint8_t data)
@@ -38,23 +41,182 @@ static int ltr390_read_byte(const struct device *dev, uint8_t reg, uint8_t *buff
 	return ret;
 }
 
-static int ltr390_init(const struct device *dev)
+static int ltr390_read_data(const struct device *dev, uint8_t reg, 
+		uint8_t *buff, size_t data_len)
 {
 	const struct ltr390_config *config = dev->config;
-	struct ltr390_data *data = dev->data;
 
-	if (!device_is_ready(config->i2c.bus)) {
-		LOG_ERR("Bus device is not ready");
-		return -ENODEV;
+	int ret = i2c_write_read_dt(&config->i2c, &reg, sizeof(reg), buff,
+			data_len);
+
+	if (ret < 0) {
+		LOG_ERR("Read reg %x error: %d", reg, ret);
+		return ret;
 	}
 
+	return ret;
+}
+
+static int ltr390_check_id(const struct device *dev)
+{
 	uint8_t buff = 0;
 	int ret = 0;
 
 	ret = ltr390_read_byte(dev, LTR390_PART_ID, &buff);
-	if (buff != LTR390_ID || ret < 0) {
-		LOG_ERR("Can't read ID: %d", ret);
+	if (ret < 0)
+	{
+		return ret;
+	}
+	else
+	if (buff != LTR390_ID)
+	{
 		return -EINVAL;
+	}
+
+	return 0;
+}
+
+static int ltr390_enable(const struct device *dev)
+{
+	uint8_t ctl_reg_val = 0;
+	int ret;
+
+	ret = ltr390_read_byte(dev, LTR390_MAIN_CTRL, &ctl_reg_val);
+	if (ret < 0)
+	{
+		return ret;
+	}
+
+	ctl_reg_val |= (1 << 1); // enable bit
+
+	ret = ltr390_write_byte(dev, LTR390_MAIN_CTRL, ctl_reg_val);
+	if (ret < 0)
+	{
+		return ret;
+	}
+
+	return 0;
+}
+
+static int ltr390_set_gain(const struct device *dev, enum ltr390_gain gain)
+{
+	int ret;
+
+	if (gain > GAIN_18)
+	{
+		LOG_ERR("Wrong gain: %d", gain);
+		return -EINVAL;
+	}
+
+	ret = ltr390_write_byte(dev, LTR390_GAIN, gain);
+	if (ret < 0)
+	{
+		return ret;
+	}
+
+	return 0;
+}
+
+
+static int ltr390_set_rate(const struct device *dev, enum ltr390_rate rate)
+{
+	int ret;
+
+	if (ret > RATE_2000MS)
+	{
+		return -EINVAL;
+	}
+
+	uint8_t meas_rate = 0;
+	ret = ltr390_read_byte(dev, LTR390_MEAS_RATE, &meas_rate);
+	if (ret < 0)
+	{
+		return ret;
+	}
+
+	meas_rate = (meas_rate & 0x8F) | rate;
+
+	ret = ltr390_write_byte(dev, LTR390_MEAS_RATE, meas_rate);
+	if (ret < 0)
+	{
+		return ret;
+	}
+
+	return 0;
+}
+
+static int ltr390_set_resolution(const struct device *dev, 
+		enum ltr390_resolution resolution)
+{
+	int ret;
+
+	if (resolution > RESOLUTION_13BIT_TIME12_5MS)
+	{
+		return -EINVAL;
+	}
+
+	uint8_t meas_rate = 0;
+	ret = ltr390_read_byte(dev, LTR390_MEAS_RATE, &meas_rate);
+	if (ret < 0)
+	{
+		return ret;
+	}
+
+	meas_rate = (meas_rate & 0x8F) | resolution << 4;
+
+	ret = ltr390_write_byte(dev, LTR390_MEAS_RATE, meas_rate);
+	if (ret < 0)
+	{
+		return ret;
+	}
+
+	return 0;
+}
+
+static int ltr390_init(const struct device *dev)
+{
+	const struct ltr390_config *config = dev->config;
+	int ret;
+
+	if (!device_is_ready(config->i2c.bus))
+	{
+		LOG_ERR("Bus device is not ready");
+		return -ENODEV;
+	}
+
+	ret = ltr390_check_id(dev);
+	if (ret < 0)
+	{
+		LOG_ERR("Check ID failed: %d", ret);
+		return ret;
+	}
+
+	ret = ltr390_enable(dev);
+	if (ret < 0)
+	{
+		LOG_ERR("Enable failed: %d", ret);
+		return ret;
+	}
+
+	ret = ltr390_set_gain(dev, GAIN_3);
+	if (ret < 0)
+	{
+		LOG_ERR("Set gain failed: %d", ret);
+		return ret;
+	}
+
+	ret = ltr390_set_rate(dev, RATE_100MS);
+	if (ret < 0)
+	{
+		LOG_ERR("Set rate failed: %d", ret);
+		return ret;
+	}
+
+	ret = ltr390_set_resolution(dev, RESOLUTION_18BIT_TIME100MS);
+	if (ret < 0)
+	{
+		LOG_ERR("Set resolution failed: %d", ret);
+		return ret;
 	}
 
 	return 0;
--- src/ltr390.h
+++ src/ltr390.h
@@ -18,34 +18,44 @@
 #define LTR390_THRESH_UP	    (0x21) // Upper threshold, low byte, 3 byte
 #define LTR390_THRESH_LOW	    (0x24) // Lower threshold, low byte, 3 byte
 
-//ALS/UVS measurement resolution, Gain setting, measurement rate
-#define RESOLUTION_20BIT_TIME400MS  (0X00)
-#define RESOLUTION_19BIT_TIME200MS  (0X10)
-#define RESOLUTION_18BIT_TIME100MS  (0X20) //default
-#define RESOLUTION_17BIT_TIME50MS   (0x3)
-#define RESOLUTION_16BIT_TIME25MS   (0x40)
-#define RESOLUTION_13BIT_TIME12_5MS (0x50)
-#define RATE_25MS		    (0x0)
-#define RATE_50MS		    (0x1)
-#define RATE_100MS		    (0x2) //default
-#define RATE_200MS		    (0x3)
-#define RATE_500MS		    (0x4)
-#define RATE_1000MS		    (0x5)
-#define RATE_2000MS		    (0x6)
-
 //measurement Gain Range.
-#define GAIN_1			    (0x0)
-#define GAIN_3			    (0x1) // default
-#define GAIN_6			    (0x2)
-#define GAIN_9			    (0x3)
-#define GAIN_18			    (0x4)
 
 #define LTR390_ID		    (0xb2)
+
+enum ltr390_gain {
+	GAIN_1 = 0x0,
+	GAIN_3 = 0x1,
+	GAIN_6 = 0x2,
+	GAIN_9 = 0x3,
+	GAIN_18 = 0x4,
+};
+
+enum ltr390_rate {
+	RATE_25MS = (0x0),
+	RATE_50MS = (0x1),
+	RATE_100MS = (0x2),
+	RATE_200MS = (0x3),
+	RATE_500MS = (0x4),
+	RATE_1000MS = (0x5),
+	RATE_2000MS = (0x6),
+};
+
+enum ltr390_resolution {
+	RESOLUTION_20BIT_TIME400MS = 0X00,
+	RESOLUTION_19BIT_TIME200MS = 0X10,
+	RESOLUTION_18BIT_TIME100MS = 0X20,
+	RESOLUTION_17BIT_TIME50MS = 0x3,
+	RESOLUTION_16BIT_TIME25MS = 0x40,
+	RESOLUTION_13BIT_TIME12_5MS = 0x50,
+};
 
 struct ltr390_config {
 	struct i2c_dt_spec i2c;
 };
 
-struct ltr390_data {};
+struct ltr390_data {
+	uint8_t uvi;
+	uint32_t lux;
+};
 
 #endif /* LTR390_DISPLAY_DRIVER_H__ */
